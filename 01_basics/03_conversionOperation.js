ec130082dd21fbc4160973041fbde0153ca01606
21VISHAL3 <168293942+21VISHAL3@users.noreply.github.com>
2024-12-30 18:39:07 +0000

    conersion

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,24 @@
+let score = "33abc"
+
+console.log(typeof score);
+console.log(typeof(score));
+
+let valueInNumber = Numer(score)
+console.log(typeof valueInNumber);
+console.log(valueInNumebr);
+
+// "33" => 33
+//  "33abc" => NaN
+//  true => 1;
+
+let isLoggedIn = true
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn);
+
+// 1 => true; 0 => false
+// "" => false
+//  "vishal" => true
+
+let someNumebr = 33
+let stringNumber = String(soemNumber)
+console.log(typeof stringNumber);
