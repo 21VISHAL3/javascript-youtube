2fa6472907c1ecb1236d7b52b20b30931db8ee02
21VISHAL3 <168293942+21VISHAL3@users.noreply.github.com>
2025-07-17 22:02:04 +0000

    datatypes

--- 01_basics/dataTypes_summary.js
+++ 01_basics/dataTypes_summary.js
@@ -1,6 +1,7 @@
 // prefer ecmaScript // types of Operator
 // link https://262.ecma-international.org/5.1/#sec-11.4.3
-// primitive  
+
+// primitive  data types
 // 7 types : String, Number, Boolean, null, undefined, Symbol, BigInt
 
 const score = 100
